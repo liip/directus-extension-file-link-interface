2a5f9462fb976e8d8f4a30a2c984b1460056869c
Leo Roettele <leo.roettele@liip.ch>
2022-12-23 14:14:06 +0100

    add migration

---
+++ src/20221222FILELINK-add-file-link-field.js
@@ -0,0 +1,30 @@
+module.exports = {
+  async up(knex) {
+    await knex('directus_fields').insert([
+      {
+        collection: 'directus_files',
+        field: 'file_link',
+        special: 'alias,no-data',
+        interface: 'custom',
+        options: null,
+        display: null,
+        display_options: null,
+        readonly: 0,
+        hidden: 0,
+        sort: null,
+        width: 'full',
+        translations: null,
+        note: null,
+        conditions: null,
+        required: 0,
+        group: null,
+        validation: null,
+        validation_message: null
+      },
+    ]);
+  },
+
+  async down(knex) {
+    await knex('directus_fields').where('collection', 'directus_files').del()
+  },
+};
