eb2def4e1bee11aa49713a40d6524fd9350a774e
Leo Roettele <leo.roettele@liip.ch>
2023-01-03 14:07:57 +0100

    better name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
 	id: 'custom',
-	name: 'Public-File-Link',
+	name: 'Public File Link',
 	icon: 'box',
 	description: 'This interface displays the filelink',
 	component: InterfaceComponent,
