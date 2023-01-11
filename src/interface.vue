c90b26e7f2f10a89419930988528095438f0641f
Leo Roettele <leo.roettele@liip.ch>
2023-01-11 16:39:20 +0100

    use inject for the file data instead of useApi

--- src/interface.vue
+++ src/interface.vue
@@ -18,40 +18,13 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useApi } from '@directus/extensions-sdk';
-import { ref, watch, watchEffect } from 'vue';
+import { inject, ref } from 'vue';
 import { getPublicURL } from './utils';
-import { FileData, FileResponse } from './types';
 import { useI18n } from 'vue-i18n';
 
-const props = defineProps({
-  collection: {
-    type: String,
-    required: true,
-  },
-  field: {
-    type: String,
-    required: true,
-  },
-  primaryKey: {
-    type: Number
-  }
-});
-
-const api = useApi();
-const fileData= ref<FileData | null>(null);
-const fileLink = ref<string>('');
 const baseUrl: string = getPublicURL();
-
-watchEffect(async () => {
-  const response: FileResponse = await api.get(`/files/?filter[id][_eq]=${props.primaryKey}`);
-  const data: FileData = response.data.data[0];
-  fileData.value = data;
-});
-
-watch([fileData], () => {
-  fileLink.value = `${baseUrl}assets/${fileData.value?.id}/${fileData.value?.filename_download}`
-});
+const fileData = inject('values', ref<Record<string, any>>({}));
+const fileLink: string = `${baseUrl}assets/${fileData.value?.id}/${fileData.value?.filename_download}`
 
 const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text)
