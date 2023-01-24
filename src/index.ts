c303f0be50fee1d34e545a8dfa7a7bc1bf2422a3
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-01-24 17:56:03 +0100

    fix extension id

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
-	id: 'extension-public-file-link',
+	id: 'extension-file-link',
 	name: 'Public File Link',
 	icon: 'box',
 	description: 'This interface displays the public link of a file.',
